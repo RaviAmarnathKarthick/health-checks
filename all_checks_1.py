82ec2d8d15dbfe9467683b1e744d64c6b4c71827
Ravi Amarnath Karthick <trakarthick@gmail.com>
2022-02-25 00:25:37 +0530

    new fiel

---
+++ all_checks_1.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+import os
+import sys
+
+def check_reboot():
+    """Returns true if the computer has a pending reboot"""
+    return os.path.exist("/run/reboot-required")
+
+
+def check_disk_full(disk, min_gb, min_percent):
+    """Returns Truw if there isn't enough disk space, False otherwise."""
+    # Calculate the percent of free space
+    percent_free = 100* du.free /du.total
+    # Calculate how many fre gigabytes
+    gigabytes_free = du.free / 2**30
+    if percent _free < min_percent or gigabytes_free < min_gb:
+        return True
+    return False
+
+def main()
+  if check_reboot():
+      print("Pending Reboot.")
+      sys.exit(1)
+  if check_disk_full(disk="/",min_gb=2, min_percent=10):
+     print("Disk Full.")
+     sys.exit(1)
+ print("Everything ok.")
+ sys.exit(0)
+main()
